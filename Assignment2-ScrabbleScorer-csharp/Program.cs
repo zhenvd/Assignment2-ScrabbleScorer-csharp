6aa9908d470d962e3c675e74b478dc018ef23607
Vincent Zhen <Vincent.D.Zhen@Outlook.com>
2021-06-24 11:07:45 -0400

    Added and tested InitialPrompt method

--- Assignment2-ScrabbleScorer-csharp/Program.cs
+++ Assignment2-ScrabbleScorer-csharp/Program.cs
@@ -55,7 +55,45 @@ namespace Assignment2_ScrabbleScorer_csharp
 
         //Code your InitialPrompt method here
 
+        static int InitialPrompt()
+        {
+            Console.WriteLine("Welcome to the Scrabble score calculator!" +
+                "\nWhich scoring algorithm would you like to use?" +
+                "\n1 - Scrabble: The traditional scoring algorithm." +
+                "\n2 - Simple Score: Each letter is worth 1 point." +
+                "\n3 - Bonus Vowels: Vowels are worth 3 pts, and consonants are 1 pt." +
+                "\n\nEnter 1, 2 or 3: ");
+            string choiceString = Console.ReadLine();
+            /*int intChoice = int.Parse(choiceString);*/
+            int intChoice;
+            int output = 0;
+            bool exit = true;
+            while(exit)
+            {
+                if (int.TryParse(choiceString, out intChoice))
+                {
+                    while (intChoice < 1 || intChoice > 3)
+                    {
+                        Console.WriteLine("Please enter 1, 2 or 3: ");
+                        choiceString = Console.ReadLine();
+                        if (int.TryParse(choiceString, out intChoice))
+                        {
+                            
+                        }
+                    }
+                    exit = false; //exits while loop
+                    output = intChoice;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a numeric value.");
+                    choiceString = Console.ReadLine();
+                }
+            }
 
+            return output;
+
+        }
 
 
 
@@ -70,7 +108,7 @@ namespace Assignment2_ScrabbleScorer_csharp
         static void Main(string[] args)
         {
             //Call your RunProgram method here
-
+            InitialPrompt();
 
 
 
